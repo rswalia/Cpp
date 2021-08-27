47a66f280122e00df23f5c9872e98ace862f215f
Rohit <waliarohit2001@gmail.com>
2021-08-27 21:13:55 +0530

    Initial commit

---
+++ Advance/1.cpp
@@ -0,0 +1,38 @@
+//C++ = C + Oops
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// typedef struct animal
+// {
+//     int id;
+//     char name;
+// } an;
+
+class Employee 
+{
+    private :
+        int a,b,c;
+    public :
+        int d,e;
+        void setData(int a1,int b1,int c1);//Declaration 
+        void getData(){
+            cout<<"The value of a "
+        }
+}
+
+
+int main(){
+    // an dog;
+    // an cat;
+    // dog.id=1;
+    // cat.id=2;
+    // dog.name='d';
+    // cat.name='c';
+    // cout<<dog.id<<endl;
+    // cout<<dog.name<<endl;
+    // cout<<cat.id<<endl;
+    // cout<<cat.name<<endl;
+    
+    return 0;
+}
---
+++ Basic/1_first.cpp
@@ -0,0 +1,16 @@
+//********** 1 ************//
+
+#include <iostream>   //header file
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+} 
+// Single line comment
+
+/*
+multi
+line
+comment
+*/
